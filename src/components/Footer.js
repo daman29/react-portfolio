85718b6ff30416cd430c14af891a16d9809dde36
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-08-02 12:08:34 +0930

    added footer images and stylesheet

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,35 @@
-import { FooterContainer } from "../styles/FooterStyle";
+import {
+  ContentContainer,
+  FooterContainer,
+  FooterImage,
+  SocialContainer,
+} from "../styles/FooterStyle";
+
+import emailImage from "../assets/images/email.png";
+import linkedinImage from "../assets/images/linkedin.png";
+import githubImage from "../assets/images/github.png";
+import phoneImage from "../assets/images/phone.png";
 
 export default function Footer() {
-  return <FooterContainer>Footer</FooterContainer>;
+  return (
+    <FooterContainer>
+      <ContentContainer>
+        Footer
+        <SocialContainer>
+          <a href="mailto:damneet.sambhy@hotmail.com" target="_blank">
+            <FooterImage src={emailImage}></FooterImage>
+          </a>
+          <a href="https://www.linkedin.com/in/damneet-sambhy/" target="_blank">
+            <FooterImage src={linkedinImage}></FooterImage>
+          </a>
+          <a href="https://github.com/daman29" target="_blank">
+            <FooterImage src={githubImage}></FooterImage>
+          </a>
+          <a href="tel:+61450088300" target="_blank">
+            <FooterImage src={phoneImage}></FooterImage>
+          </a>
+        </SocialContainer>
+      </ContentContainer>
+    </FooterContainer>
+  );
 }
--- src/styles/FooterStyle.js
+++ src/styles/FooterStyle.js
@@ -2,10 +2,32 @@ import styled from "styled-components";
 import { Pallette } from "../assets/ColourPallette";
 
 export const FooterContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    min-height: 80px;
-    width: 100%;
-    background-color: ${Pallette.primary};
+  display: flex;
+  justify-content: center;
+  min-height: 80px;
+  width: 100%;
+  background-color: ${Pallette.highlight};
+`;
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  max-width: 1500px;
+`;
 
-`
+export const SocialContainer = styled.div`
+  display: flex;
+  width: 80%;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
+
+export const FooterImage = styled.img`
+  height: 60px;
+  filter: grayscale(100%);
+  &:hover {
+    filter: grayscale(0%);
+  }
+`;
