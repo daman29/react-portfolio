1aadfe5f8173e03ab1e7af1f02fae73ef7aeda07
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-08-03 16:20:31 +0930

    added basic sizing to the project cards

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,13 +1,18 @@
 import { ProjectsArray } from "../data/Projects";
 import Project from "../components/Project";
 import { CardContainer } from "../styles/ProjectStyle";
+import { ContentContainer } from "../styles/PageStyle";
+import { useEffect } from "react";
 
 export function Projects() {
   return (
-    <CardContainer>
-      {ProjectsArray.map((project) => (
-        <Project project={project} />
-      ))}
-    </CardContainer>
+    <ContentContainer>
+      <h2>Projects</h2>
+      <CardContainer>
+        {ProjectsArray.map((project) => (
+          <Project project={project} />
+        ))}
+      </CardContainer>
+    </ContentContainer>
   );
 }
--- src/styles/ProjectStyle.js
+++ src/styles/ProjectStyle.js
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 import { Pallette } from "../assets/ColourPallette";
 
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  max-width: 1400px;
+`;
+
 export const CardContainer = styled.div`
     width: 100%;
     display: flex;
@@ -15,4 +24,10 @@ export const ProjectCard = styled.div`
     margin: 1%;
     background-color: ${Pallette.primaryLight};
     min-height: 30vh;
+    @media (max-width: 700px){
+        flex: 1 1 48%;
+    }
+    @media (max-width: 500px){
+        flex: 1 1 98%;
+    }
 `
